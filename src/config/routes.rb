277131d54acd61e59a17350be7c32ce1c20953f4
masaki2355 <takoyaki2355@icloud.com>
2021-09-02 16:31:42 +0900

    コントローラー作成

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   get 'heart/home'
   get 'heart/call'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
