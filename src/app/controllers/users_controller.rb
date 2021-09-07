79a11980290d900bd7df87b6bc9cbfe202551d87
masaki2355 <takoyaki2355@icloud.com>
2021-09-07 20:09:00 +0900

    リベース

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+<<<<<<< HEAD
   def index
   end
+=======
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+>>>>>>> 2051b08 (ページ追加)
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'heart/home'
   get 'heart/call'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
