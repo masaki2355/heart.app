b2aa4fdeff6d0a55dea12759b2f71cd5478df02a
masaki2355 <takoyaki2355@icloud.com>
2021-09-12 00:41:48 +0900

    モデル関連付途中

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -9,9 +9,18 @@ class PostsController < ApplicationController
   end
   
   def create
+    @post = Post.new(params.require(:post).permit(:content, :user_id))
+      if @post.save
+        flash[:notice] = "新規投稿をしました"
+        redirect_to :users
+      else
+        render"posts"
+      end
   end
   
   def show
+    @user = User.find(params[:id])
+    @post = Post.new
   end
   
   def edit
